2eae25c517acc7bdc2bd2f56ceff74f63d860d2b
blaskodaniel <blaskoi@gmail.com>
2021-04-22 16:20:02 +0200

    modify rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve'; // for node_modules dependecies
 import external from 'rollup-plugin-peer-deps-external'; // expect packages in peerDependecies
 import postcss from 'rollup-plugin-postcss'; // for css
 import { terser } from 'rollup-plugin-terser'; // for minify
+
+const extensions = ['.js', '.jsx']
 
 export default [
     {
@@ -16,19 +18,27 @@ export default [
                 file: 'dist/index.es.js',
                 format: 'es',
                 exports: 'named',
-            }
+            },
+            {
+                file: 'dist/index.esm.js',
+                format: 'esm',
+                sourcemap: true,
+            },
         ],
         plugins: [
+			external(),
             postcss({
                 plugins: [],
                 minimize: true
             }),
+			resolve(extensions),
             babel({
+                babelHelpers: 'bundled',
                 exclude: 'node_modules/**',
+				include: ['src/**/*.js(x)?'],
+				extensions,
                 presets: ['@babel/preset-react']
             }),
-            external(),
-            resolve(),
             terser()
         ]
     }
